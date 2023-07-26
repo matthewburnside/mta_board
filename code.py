f71449260e585f3c9c4c61c6bbf8fc4511159e58
matthewburnside <mb@maclaybridge.com>
2023-07-26 13:19:41 -0400

    Reduce memory footprint

--- code.py
+++ code.py
@@ -1,25 +1,24 @@
-import traceback
-
-
-import adafruit_display_text.label
 import displayio
+import gc
 import microcontroller
 import time
+import traceback
 from adafruit_bitmap_font import bitmap_font
 from adafruit_datetime import datetime
 from adafruit_display_text import label
-from adafruit_display_shapes import circle
-from adafruit_display_shapes import line
+from adafruit_display_shapes import circle, line
 from adafruit_matrixportal.matrix import Matrix
 from adafruit_matrixportal.network import Network
 from board import NEOPIXEL
 from secrets import secrets
 
-ERROR_THRESHOLD   = 3     # before resetting the microcontroller
-REFRESH_RATE      = 15    # time (s) between refreshing the counts
-TIME_SYNC_RATE    = 3600  # query the network time once per hour 
-WEATHER_SYNC_RATE = 600   # check weather every 10 mins
-MAX_ENTRIES       = 3     # count of arrivals to show
+ERROR_THRESHOLD    = 3     # before resetting the microcontroller
+REFRESH_RATE       = 10    # time (s) between refreshing the counts
+TRAIN_LIMIT        = 0     # no rate limit on the train API
+BUS_LIMIT          = 31    # refresh the bus every 31s (there's a 30s rate limit)
+TIME_LIMIT         = 600   # resync the clock every 10 mins
+WEATHER_LIMIT      = 600   # refresh the weather every 10 mins
+MAX_ENTRIES        = 3     # count of arrivals to show
 
 MONTH = ['','Jan','Feb','Mar','Apr','May','Jun',
     'Jul','Aug','Sep','Oct','Nov','Dec']
@@ -33,7 +32,6 @@ BLACK  = 0x000000
 
 FONT = {
     '5x7': bitmap_font.load_font("fonts/5x7.bdf"),
-    '6x10': bitmap_font.load_font("fonts/6x10.bdf"),
     'helvB10': bitmap_font.load_font("fonts/helvB10.bdf"),
     'thumb': bitmap_font.load_font("fonts/tom-thumb.bdf"),
 }
@@ -44,7 +42,7 @@ display.rotation = 270
 network          = Network(status_neopixel=NEOPIXEL, debug=False)
 
 
-### Trains setup
+### Trains API setup
 
 TRAIN_API = 'https://api.wheresthefuckingtrain.com/by-id/'
 STATION = {
@@ -57,13 +55,11 @@ def train_api(station, route, dir):
     schedule = network.fetch_data(query, json_path=(["data"],))
     arrivals = []
     now = datetime.now()
-
     for entry in schedule:
         trains = entry[dir] # only grab the trains in the dir we want
         trains = [t for t in trains if t['route'] == route] # only our line
         for train in trains:
             arrivals.append(in_mins(now, train['time']))
-
     arrivals.sort()
     arrivals = ["Ar" if i < 1 else str(i) for i in arrivals]
     print("%s %s %s: %s" % (station, route, dir, arrivals))
@@ -75,37 +71,37 @@ def in_mins(now, date_str):
 
 
 
-### Buses setup
+### Buses API setup
 
 BUS_API = 'https://bustime.mta.info/api/siri/stop-monitoring.json' \
     '?key=%s&MonitoringRef=%s&DirectionRef=%s&MaximumStopVisits=%s'
-
 STOP = {
     'GATES AV/GRANDVIEW AV': '504111',
 }
 
 def bus_api(stop, dir):
     query = BUS_API % (secrets['bustime_key'], STOP[stop], dir, MAX_ENTRIES)
+    gc.collect()
     schedule = network.fetch_data(query, json_path=(["Siri"],))
-    buses = schedule['ServiceDelivery']['StopMonitoringDelivery'][0]
-    journeys = buses['MonitoredStopVisit']
-
+    journeys = schedule['ServiceDelivery']['StopMonitoringDelivery'] \
+        [0]['MonitoredStopVisit']
     stops = []
     for j in journeys:
-        bus = j['MonitoredVehicleJourney']['MonitoredCall']['Extensions']
-        stops.append(bus['Distances']['StopsFromCall'])
+        stops.append(j['MonitoredVehicleJourney']['MonitoredCall'] \
+            ['Extensions']['Distances']['StopsFromCall'])
 
     print("%s %s %s" % (stop, dir, stops))
     stops.sort()
     stops = [str(i) for i in stops]
+    del schedule
     return stops[:MAX_ENTRIES]
 
 
 
-### Weather setup
+### Weather API setup
 
 WEATHER_API = 'https://api.openweathermap.org/data/2.5/weather?' \
-    'lat=%s&lon=%s&appid=%s&units=imperial'
+    'lat=%s&lon=%s&appid=%s&units=imperial&StopMonitoringDetailLevel=minimum'
 
 ICONS_FILE = displayio.OnDiskBitmap('weather-icons.bmp')
 ICON_DIM = (16, 16) # width x height
@@ -131,9 +127,7 @@ def get_sprite(icon):
     return SPRITE    
 
 def weather_api(coords):
-    print("weather")
     query = WEATHER_API % (coords['lat'], coords['lon'], secrets['weather_key'])
-    print(query)
     resp = network.fetch_data(query, json_path=([]))
     icon = resp['weather'][0]['icon']
     temp = round(resp['main']['temp'])
@@ -175,9 +169,9 @@ times = {
 }
 
 weather = {
-    'icon': displayio.Group(x=0, y=0),
-    'temp': label.Label(FONT['helvB10'], color=WHITE, x=17, y=7, text="00"),
-    'degree': label.Label(FONT['6x10'], color=WHITE, x=27, y=6, text='°'),
+    'icon':   displayio.Group(x=0, y=0),
+    'temp':   label.Label(FONT['helvB10'], color=WHITE, x=17, y=7, text="00"),
+    'degree': circle.Circle(outline=WHITE, fill=BLACK, x0=29, y0=3, r=1),
 }
 weather['icon'].append(get_sprite('01n'))
 
@@ -198,33 +192,49 @@ root_group.append(weather_group);
 display.show(root_group)
 
 
+def clock_time():
+    network.get_local_time()
+
+def m_train():
+    m_train = train_api('Forest Av', route='M', dir='N')
+    times['M'].text = "\n".join(m_train)
+
+def l_train():
+    l_train = train_api('Myrtle - Wyckoff Avs', route='L', dir='N')
+    times['L'].text = "\n".join(l_train)
+
+def b13_bus():
+    b13 = bus_api('GATES AV/GRANDVIEW AV', dir=0)
+    times['B'].text = "\n".join(b13)
+
+def weather_card():
+    icon, temp = weather_api(secrets['coords'])
+    weather['icon'].pop()
+    weather['icon'].append(get_sprite(icon))
+    weather['temp'].text = str(temp)
+
+def rate_limit(name, source, rate, last):
+    if last == None or time.monotonic() - last >= rate:
+        source()
+        gc.collect()
+        return time.monotonic() 
+    else:
+        return last
+
 errors = 0
-time_refresh = None
-weather_refresh = None
+train_last = bus_last = clock_last = weather_last = None
+network.get_local_time()
+
 while True:
     try:
-        if time_refresh == None or time.monotonic() - time_refresh > TIME_SYNC_RATE:
-            network.get_local_time()
-            time_refresh = time.monotonic()
-
         now = datetime.now()
         clock['time'].text = "%02d:%02d" % (now.hour, now.minute)
 
-        m_train = train_api('Forest Av', route='M', dir='N')
-        times['M'].text = "\n".join(m_train)
-
-        l_train = train_api('Myrtle - Wyckoff Avs', route='L', dir='N')
-        times['L'].text = "\n".join(l_train)
-
-        stops = bus_api('GATES AV/GRANDVIEW AV', dir=0)
-        times['B'].text = "\n".join(stops)
-
-        if weather_refresh == None or time.monotonic() - weather_refresh > WEATHER_SYNC_RATE:
-	    icon, temp = weather_api(secrets['coords'])
-            weather['icon'].pop()
-            weather['icon'].append(get_sprite(icon))
-            weather['temp'].text = str(temp)
-            weather_refresh = time.monotonic()
+        clock_last   = rate_limit("clock", clock_time, TIME_LIMIT, clock_last)
+        train_last   = rate_limit("m_train", m_train, TRAIN_LIMIT, train_last)
+        train_last   = rate_limit("l_train", l_train, TRAIN_LIMIT, train_last)
+        bus_last     = rate_limit("b13_bus", b13_bus, BUS_LIMIT, bus_last)
+        weather_last = rate_limit("weather", weather_card, WEATHER_LIMIT, weather_last)
 
     except Exception as e:
         print("\nError: ", e)
@@ -233,6 +243,7 @@ while True:
         if errors > ERROR_THRESHOLD:
             microcontroller.reset()
 
+    gc.collect()
+    print("Memory:\t\t%s" % (gc.mem_free()))
     time.sleep(REFRESH_RATE);
-
 
